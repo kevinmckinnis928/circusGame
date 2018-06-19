7c2658d162fea608f34239ce7a2ac1d07e1a995d
JarrettKoran <jarrettkoran@gmail.com>
2018-06-19 11:20:09 -0700

    Pause Menu

---
+++ CircusGame/Assets/Scripts/pauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour {
+
+    public static bool gamePaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("EscapePressed");
+            gamePaused = !gamePaused;
+          
+        }
+        if (!gamePaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+        void Resume ()
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            gamePaused = false;
+        }
+        void Pause ()
+        {
+            Debug.Log("Paused");
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            gamePaused = true;
+
+        }
+
+	
+}
