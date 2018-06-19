39f7bcfad5ae809864609e069f2aab125bc10dd7
JarrettKoran <jarrettkoran@gmail.com>
2018-06-19 12:26:46 -0700

    main menu

---
+++ CircusGame/Assets/Scripts/menuTop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuTop : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerExit2D(Collider2D other)
+    {     
+            Destroy(other.gameObject);
+    }
+}
